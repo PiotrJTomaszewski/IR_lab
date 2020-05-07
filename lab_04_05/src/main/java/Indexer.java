d83aee60b4e7db40b9509c0f2ca7eb2e0848682b
Piotr J. Tomaszewski <pjtomaszewski98@gmail.com>
2020-05-07 17:12:25 +0200

    Ex 1 done

--- lab_04_05/src/main/java/Indexer.java
+++ lab_04_05/src/main/java/Indexer.java
@@ -14,30 +14,26 @@ import org.xml.sax.SAXException;
 import java.io.*;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Objects;
 
-public class Indexer
-{
-    public static void main(String args[])
-    {
+public class Indexer {
+    public static void main(String[] args) {
         Indexer indexer = new Indexer();
         indexer.indexDocuments();
     }
 
-    private void indexDocuments()
-    {
+    private void indexDocuments() {
         // REMOVE PREVIOUSLY GENERATED INDEX (DONE)
-        try
-        {
+        try {
             FileUtils.deleteDirectory(new File(Constants.index_dir));
-        } catch (IOException ignored)
-        {
+        } catch (IOException ignored) {
         }
 
-        // LOAD HTML DOCUMENTS (TODO)
+        // LOAD HTML DOCUMENTS (TODO) - DONE
         ArrayList<Document> documents = getHTMLDocuments();
 
 
-        // CONSTRUCT INDEX (TODO)
+        // CONSTRUCT INDEX (TODO) - DONE
         // - Firstly, create Analyzer object (StandardAnalyzer).
         //   (An Analyzer builds TokenStreams, which analyze text.
         //   It thus represents a policy for extracting index terms from text.)
@@ -45,25 +41,30 @@ public class Indexer
         // - Construct IndexWriter (you can use FSDirectory.open and Paths.get + Constants.index_dir
         // - Add documents to the index
         // - Commit and close the index.
-
         // ----------------------------------
-        
+        StandardAnalyzer analyzer = new StandardAnalyzer();
+        IndexWriterConfig config = new IndexWriterConfig(analyzer);
+        IndexWriter writer;
+        try {
+            writer = new IndexWriter(FSDirectory.open(Paths.get(Constants.index_dir)), config);
+            writer.addDocuments(documents);
+            writer.commit();
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("Error while creating index: " + e.getMessage());
+        }
         // ----------------------------------
-
     }
 
 
-    private ArrayList<Document> getHTMLDocuments()
-    {
+    private ArrayList<Document> getHTMLDocuments() {
         // This method is finished. Find getHTMLDocument
         File dir = new File("pages");
         File[] files = dir.listFiles();
-        if (files != null)
-        {
+        if (files != null) {
             ArrayList<Document> htmls = new ArrayList<>(files.length);
-            for (int id = 0; id < files.length; id++)
-            {
-                System.out.println("Loading "+ files[id].getName());
+            for (int id = 0; id < files.length; id++) {
+                System.out.println("Loading " + files[id].getName());
                 // TODO finish getHTML document
                 htmls.add(getHTMLDocument("pages/" + files[id].getName(), id));
             }
@@ -72,8 +73,7 @@ public class Indexer
         return null;
     }
 
-    private Document getHTMLDocument(String path, int id)
-    {
+    private Document getHTMLDocument(String path, int id) {
         File file = new File(path);
         Document document = new Document();
 
@@ -109,60 +109,57 @@ public class Indexer
 
         // For the following, it is suggested to use TextField and StoredField
 
-        // TODO create a field that is stored but not indexed
+        // TODO create a field that is stored but not indexed - DONE
         // and contains document's id
         // IMPORTANT NOTE: use static fields of Constants class to get
         // the keys of the fields (e.g., Constants.id) !
         // ----------------------------------
-       
-        // ----------------------------------
+        document.add(new StoredField(Constants.id, id));
 
-        // TODO create a field that is indexed but not stored
+        // ----------------------------------
+        // TODO create a field that is indexed but not stored - DONE
         // and contains document's content
         // for this purpose, extract text from the document
         // using Tika ( use getTextFromHTMLFile() <- this method is finished )
         // ----------------------------------
-       
-        // ----------------------------------
+        String fileContent = Objects.requireNonNull(getTextFromHTMLFile(file));
+        document.add(new TextField(Constants.content, fileContent, Field.Store.NO));
 
-        // TODO create a field that is stored and indexed
+        // ----------------------------------
+        // TODO create a field that is stored and indexed - DONE
         // and contains file name
         // ----------------------------------
-       
-        // ----------------------------------
+        document.add(new TextField(Constants.filename, file.getName(), Field.Store.YES));
 
-        // TODO create an INT field (IntPoint) that is indexed
+        // ----------------------------------
+        // TODO create an INT field (IntPoint) that is indexed - DONE
         // and contains file size (bytes, .length())
         // ----------------------------------
+        document.add(new IntPoint(Constants.filesize, (int)file.length()));
 
         // ----------------------------------
-        // //TODO IntPoint is not stored but we want to a file size
+        // //TODO IntPoint is not stored but we want to a file size - DONE
         // ... so add another field (StoredField) that stores file size
         // ----------------------------------
+        document.add(new StoredField(Constants.filesize, file.length()));
 
         // ----------------------------------
-
-        // TODO add fields to the document object
-        // ----------------------------------
-  
+        // TODO add fields to the document object - Already done
         // ----------------------------------
 
-
+        // ----------------------------------
         return document;
 
     }
 
     // (DONE)
-    private String getTextFromHTMLFile(File file)
-    {
+    private String getTextFromHTMLFile(File file) {
         BodyContentHandler handler = new BodyContentHandler();
         Metadata metadata = new Metadata();
         FileInputStream inputStream;
-        try
-        {
+        try {
             inputStream = new FileInputStream(file);
-        } catch (FileNotFoundException e)
-        {
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
             return null;
         }
@@ -171,11 +168,9 @@ public class Indexer
 
         //Html parser
         HtmlParser htmlparser = new HtmlParser();
-        try
-        {
+        try {
             htmlparser.parse(inputStream, handler, metadata, pContext);
-        } catch (IOException | SAXException | TikaException e)
-        {
+        } catch (IOException | SAXException | TikaException e) {
             e.printStackTrace();
         }
 
